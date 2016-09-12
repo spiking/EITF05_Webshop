faaad2571a811484b27106243f8bf3dd01c7747d
djohsson <davidjohansson1993@gmail.com>
2016-09-12 11:22:26 +0200

    Comment out mysqli_connect

--- config.php
+++ config.php
@@ -4,5 +4,5 @@
    	define('DB_USERNAME', 'root');
    	define('DB_PASSWORD', 'rootpassword');
    	define('DB_DATABASE', 'database');
-   	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   	//$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
