ea81ec02690f37942fc81f575f80d954af4915d2
djohsson <davidjohansson1993@gmail.com>
2016-09-12 11:17:09 +0200

    Add login skeleton

--- login.php
+++ login.php
@@ -12,14 +12,24 @@
 	if (strlen($address) > 0 && strlen(email) > 0) {
     	//register
 	} else {
-		//try to login
-		$_SESSION['name'] = $name;
-		$_SESSION['email'] = $email;
-		$_SESSION['address'] = $address;
+		//something like this to login
+		//$sql = "SELECT salt FROM users WHERE username = '$name'";
+      	//$salt = mysqli_query($db,$sql);
+		//$hashed_password = hash('sha512', $password . $salt);
+		//$sql = "SELECT id FROM users WHERE username = '$name' AND hashed_password = '$hashed_password'";
+		//$results = mysqli_query($db,$sql);
+		//$count = mysqli_num_rows($result);
+
+		//if ($count == 1) {
+		//	session_register($name)
+		//	$_SESSION['name'] = $name;
+		//	$_SESSION['email'] = $email;
+		//	$_SESSION['address'] = $address;
+		//}
 	}
 	//redirect to page
 ?>
-
+<!--Place holder, will be removed-->
 Welcome <?php echo $name; ?><br>
 Your email address is: <?php echo $email;?><br>
 Your address is: <?php echo $address;?><br>
