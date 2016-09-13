d6e75b771511982fa860bfeb844601df75f0076c
Adam Thuvesen <a.thuvesen@gmail.com>
2016-09-13 20:08:30 +0200

    Layout changes, renamed files

--- js/login.js
+++ js/login.js
@@ -1,35 +1,63 @@
-$(window, document, undefined).ready(function() {
+$(window, document, undefined).ready(function () {
 
-  $('input').blur(function() {
-    var $this = $(this);
-    if ($this.val())
-      $this.addClass('used');
-    else
-      $this.removeClass('used');
-  });
-
-  var $ripples = $('.ripples');
-
-  $ripples.on('click.Ripples', function(e) {
-
-    var $this = $(this);
-    var $offset = $this.parent().offset();
-    var $circle = $this.find('.ripplesCircle');
-
-    var x = e.pageX - $offset.left;
-    var y = e.pageY - $offset.top;
-
-    $circle.css({
-      top: y + 'px',
-      left: x + 'px'
+    $('input').blur(function () {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
     });
 
-    $this.addClass('is-active');
+    var $ripples = $('.ripples');
 
-  });
+    $ripples.on('click.Ripples', function (e) {
 
-  $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
-  	$(this).removeClass('is-active');
-  });
+        var $this = $(this);
+        var $offset = $this.parent().offset();
+        var $circle = $this.find('.ripplesCircle');
+
+        var x = e.pageX - $offset.left;
+        var y = e.pageY - $offset.top;
+
+        $circle.css({
+            top: y + 'px'
+            , left: x + 'px'
+        });
+
+        $this.addClass('is-active');
+
+    });
+
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function (e) {
+        $(this).removeClass('is-active');
+    });
 
 });
+
+function showDiv() {
+
+    document.getElementById('emailDiv').style.display = "block"
+    document.getElementById('addressDiv').style.display = "block"
+    document.getElementById('registerText').onclick = hideDiv;
+    document.getElementById('registerText').innerHTML = "Already have an account?"
+    document.getElementById('title').innerHTML = 'Sign Up'
+    document.getElementById('loginButton').innerHTML = 'Sign Up'
+}
+
+function hideDiv() {
+
+    document.getElementById('emailDiv').style.display = "none";
+    document.getElementById('addressDiv').style.display = "none";
+    document.getElementById('registerText').onclick = showDiv;
+    document.getElementById('registerText').innerHTML = "Don't have an account?"
+    document.getElementById('title').innerHTML = 'Login'
+    document.getElementById('loginButton').innerHTML = 'Login'
+}
+
+function clearData() {
+    $("#usernameInput").val('');
+    $("#emailInput").val('');
+    $("#addressInput").val('');
+    $("#passwordInput").val('');
+    console.log("I've been called");
+}
