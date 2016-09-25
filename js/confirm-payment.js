2269b88c1973b922b5b33293ea59a2dc82e5ea73
Adam Thuvesen <a.thuvesen@gmail.com>
2016-09-25 21:43:22 +0200

    CSRF token added, validate input login client side

--- js/confirm-payment.js
+++ js/confirm-payment.js
@@ -12,37 +12,37 @@ function validateForm() {
     var creditcardReg = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})/;
 
     // Credit card should be added, annoying for testing though
-    
+
     if (usernameAndPwReg.test(username) && usernameAndPwReg.test(password) && emailReg.test(email)) {
         console.log("Input Valid");
         console.log("Check username and password");
 
-        $('#info-form').submit(function () {
+        $('#info-form').submit(function() {
             console.log("Ajax request initiated");
             $.ajax({
-                type: 'POST'
-                , url: '../php/login.php'
-                , data: $('#info-form').serialize()
-                , dataType: 'json'
-                , success: function (data) {
+                type: 'POST',
+                url: '../php/confirm-credentials.php',
+                data: $('#info-form').serialize(),
+                dataType: 'json',
+                success: function(data) {
                     if (data.error == true) {
-                        console.log('Invalid credentials.');
-                        $('#confirm-error').html('Incorrect Credentials');
-                        $('#confirm-error').fadeIn(1000);
-                        $('#confirm-error').delay(2000);
-                        $('#confirm-error').fadeOut(3000);
+                        console.log(data.msg);
+                        $('#confirm-error').html(data.msg);
+                        $('#confirm-error').fadeIn(500);
+                        $('#confirm-error').delay(3000);
+                        $('#confirm-error').fadeOut(500);
                     } else {
                         console.log('Correct credentials.');
                         window.location.href = 'payment-completed.php';
                     }
-                }
-                , beforeSend: function () {
+                },
+                beforeSend: function() {
 
-                }
-                , complete: function () {
+                },
+                complete: function() {
                     console.log('ajax call completed');
-                }
-                , error: function (exception) {
+                },
+                error: function(exception) {
                     console.log(exception);
                 }
             });
@@ -53,9 +53,18 @@ function validateForm() {
     } else {
         console.log("Invalid input");
         $('#confirm-error').html('Invalid Input');
-        $('#confirm-error').fadeIn(1000);
-        $('#confirm-error').delay(2000);
-        $('#confirm-error').fadeOut(3000);
+        $('#confirm-error').fadeIn(500);
+        $('#confirm-error').delay(3000);
+        $('#confirm-error').fadeOut(500);
         return false;
     }
 }
+
+// Modern browsers got back-forward cache (BFCache) when user click back-button, reset form
+$(document).ready(function() {
+    $(window).bind("pageshow", function() {
+        var form = $('form');
+        console.log("Reset form!");
+        form[0].reset();
+    });
+});
--- js/login-logout.js
+++ js/login-logout.js
@@ -1,72 +1,126 @@
-$('#btn-login').click(function() {
-	console.log('clicked login button');
-	$.ajax({
-		type: 'POST',
-		url: '../php/login.php',
-		data: $('#login-form').serialize(),
-		dataType: 'json',
-		success: function(data) {    
-			if (data.error == true) {
-				console.log('Invalid credentials.');
-				$('#login-error').html(data.msg);
-				$('#login-error').fadeIn(1000);
-				$('#login-error').fadeOut(3000);
-			} else {
-				console.log('Correct credentials.');
-				window.location.href = 'index.php';
-			}
-		},
-		beforeSend: function() {
+function validateFormLogin() {
 
-		},
-		complete: function() {
-			console.log('ajax call completed');
-		},
-		error: function(exception) {
-			console.log(exception);
-		}
-	});
-	return false;
-});
+    console.log("Validate User Input");
 
-$('#btn-register').click(function() {
-	console.log('clicked register button');
-	$.ajax({
-		type: 'POST',
-		url: '../php/login.php',
-		data: $('#register-form').serialize(),
-		dataType: 'json',
-		success: function(data) {    
-			if (data.error == true) {
-                console.log("Failures")
-				$('#register-error').html(data.msg);
-				$('#register-error').fadeIn(1000);
-				$('#register-error').fadeOut(3000);
-			} else {
-                console.log("Successfull reg")
-                window.location.href = 'index.php';
-				$('#register-success').html(data.msg);
-				$('#register-success').fadeIn(1000);
-				$('#register-success').fadeOut(3000);
-			}
-		},
-		beforeSend: function() {
+    var username = document.getElementById("name").value;
+    console.log(username);
 
-		},
-		complete: function() {
-            console.log('ajax call completed');
-		},
-		error: function(exception) {
-			console.log(exception);
-		}
-	});
-	return false;
-});
+    var password = document.getElementById("password").value;
+    console.log(password);
 
+    var usernameAndPwReg = /^[0-9a-zA-Z_.-]+$/;
 
-$(window, document, undefined).ready(function () {
+    if (usernameAndPwReg.test(username) && usernameAndPwReg.test(password)) {
+        console.log("Input Valid");
 
-    $('input').blur(function () {
+        $.ajax({
+            type: 'POST',
+            url: '../php/login.php',
+            data: $('#login-form').serialize(),
+            dataType: 'json',
+            success: function(data) {
+                if (data.error == true) {
+                    console.log(data.msg);
+                    $('#login-error').html(data.msg);
+                    $('#login-error').fadeIn(1000);
+                    $('#login-error').fadeOut(3000);
+                } else {
+                    console.log('Correct credentials.');
+                    window.location.href = 'index.php';
+                }
+            },
+            beforeSend: function() {
+
+            },
+            complete: function() {
+                console.log('ajax call completed');
+            },
+            error: function(exception) {
+                console.log(exception);
+            }
+        });
+        return false;
+    } else {
+
+        $('#login-error').html('Invalid Input');
+        console.log("Invalid input login form");
+
+        $('#login-error').fadeIn(500);
+        $('#login-error').fadeOut(3000);
+        $('#login-error').fadeOut(500);
+    }
+    return false;
+}
+
+function validateFormReg() {
+
+    console.log("Validate User Input");
+
+    if (!document.getElementById("nameReg") != null) {
+        var username = document.getElementById("nameReg").value;
+        console.log(username);
+    }
+    if (!document.getElementById("passwordReg") != null) {
+        var password = document.getElementById("passwordReg").value;
+        console.log(password);
+    }
+    if (document.getElementById("addressReg") != null) {
+        var address = document.getElementById("addressReg").value;
+        console.log(address);
+    }
+
+    var usernameAndPwReg = /^[0-9a-zA-Z_.-]+$/;
+    // var addressReg = /^[a-zA-Z0-9\s,'-]*$/;
+    // Add address, annoying for testing
+
+    if (usernameAndPwReg.test(username) && usernameAndPwReg.test(password)) {
+        console.log('clicked register button');
+        $.ajax({
+            type: 'POST',
+            url: '../php/login.php',
+            data: $('#register-form').serialize(),
+            dataType: 'json',
+            success: function(data) {
+                if (data.error == true) {
+                    console.log("Failures")
+                    $('#register-error').html(data.msg);
+                    $('#register-error').fadeIn(1000);
+                    $('#register-error').fadeOut(3000);
+                } else {
+                    console.log("Successfull reg")
+                    window.location.href = 'index.php';
+                    $('#register-success').html(data.msg);
+                    $('#register-success').fadeIn(1000);
+                    $('#register-success').fadeOut(3000);
+                }
+            },
+            beforeSend: function() {
+
+            },
+            complete: function() {
+                console.log('ajax call completed');
+            },
+            error: function(exception) {
+                console.log(exception);
+            }
+        });
+        return false;
+    } else {
+
+        $('#register-error').html('Invalid Input');
+        console.log("Invalid input login form");
+
+        $('#register-error').fadeIn(500);
+        $('#register-error').fadeOut(3000);
+        $('#register-error').fadeOut(500);
+    }
+
+    return false;
+}
+
+$(window, document, undefined).ready(function() {
+
+    $('input').blur(function() {
         var $this = $(this);
         if ($this.val())
             $this.addClass('used');
@@ -76,7 +130,7 @@ $(window, document, undefined).ready(function () {
 
     var $ripples = $('.ripples');
 
-    $ripples.on('click.Ripples', function (e) {
+    $ripples.on('click.Ripples', function(e) {
 
         var $this = $(this);
         var $offset = $this.parent().offset();
@@ -86,33 +140,36 @@ $(window, document, undefined).ready(function () {
         var y = e.pageY - $offset.top;
 
         $circle.css({
-            top: y + 'px'
-            , left: x + 'px'
+            top: y + 'px',
+            left: x + 'px'
         });
 
         $this.addClass('is-active');
 
     });
 
-    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function (e) {
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
         $(this).removeClass('is-active');
     });
 
 });
 
 function toggleForm(type) {
-    
+
     console.log(type)
-    
-    if(type == "register") {
+
+    if (type == "register") {
         document.getElementById('register-form').style.display = "block";
         document.getElementById('login-form').style.display = "none";
     } else {
         document.getElementById('register-form').style.display = "none";
-        document.getElementById('login-form').style.display="block";
+        document.getElementById('login-form').style.display = "block";
     }
 }
 
-function clearData() {
-
-}
+// Modern browsers got back-forward cache (BFCache) when user click back-button, reset all forms
+$(document).ready(function() {
+    $(window).bind("pageshow", function() {
+        $('form').each(function() { this.reset() });
+    });
+});
